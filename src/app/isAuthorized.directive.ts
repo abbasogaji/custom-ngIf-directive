15f03ebd5e74684cd3d7ca5016555f85752fcf5a
Abbas Ogaji <abbasogaji@gmail.com>
2020-02-05 07:09:41 +0100

    first commit

---
+++ src/app/isAuthorized.directive.ts
@@ -0,0 +1,31 @@
+import { Directive, TemplateRef,ViewContainerRef, Input, OnInit, OnChanges  } from '@angular/core';
+
+@Directive({ selector: '[isAuthorized]'})
+export class IsAuthorizedDirective implements OnInit, OnChanges{
+  @Input() isAuthorizedIn : string[]
+  @Input() isAuthorized : string
+  private hasView = false;
+
+  constructor(private templateRef: TemplateRef<any>, 
+              private vcr: ViewContainerRef) { }
+
+  ngOnInit() {
+    this.configureView()
+  }
+
+  ngOnChanges(){
+    this.configureView()
+  }
+
+  configureView(){
+    const isAuthorized = this.isAuthorizedIn.some(i => i == this.isAuthorized)
+
+    if (isAuthorized && !this.hasView) {
+        this.vcr.createEmbeddedView(this.templateRef);
+        this.hasView = true;
+    } else if (!isAuthorized && this.hasView) {
+        this.vcr.clear();
+        this.hasView = false;     
+    }
+  }
+}
